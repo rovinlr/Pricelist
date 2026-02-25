d0232aea4384a4b56909ec0a59955f854bc20b36
rovinlr <rovinlr@gmail.com>
2026-02-24 19:39:03 -0600

    [ADD] product_pricelist_costo: método de cálculo por costo

---
+++ product_pricelist_costo/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Product Pricelist Cost Method",
+    "summary": "Adds a Cost-based computation method for pricelist rules.",
+    "version": "19.0.1.0.0",
+    "category": "Sales/Sales",
+    "author": "Custom",
+    "license": "LGPL-3",
+    "depends": ["product"],
+    "data": [
+        "views/product_pricelist_item_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
